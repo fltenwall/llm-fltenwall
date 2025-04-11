4adea4b5d6d04bd3e2ea2b5ef1c14b1da6aeaa65
轩灵 <48324481+fltenwall@users.noreply.github.com>
2025-04-11 17:39:02 +0800

    first upload

---
+++ model_download.py
@@ -0,0 +1,5 @@
+# model_download.py
+from modelscope import snapshot_download
+
+model_dir = snapshot_download('LLM-Research/Llama-4-Scout-17B-16E-Instruct', cache_dir='../model', revision='master')
+# model_dir = snapshot_download('LLM-Research/Llama-4-Maverick-17B-128E-Instruct-FP8', cache_dir='请修改我！', revision='master')
